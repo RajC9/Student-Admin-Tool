83c8634b1f7e00ab1d46017c7a7b58a2df0fe14d
RajC9 <65564298+RajC9@users.noreply.github.com>
2020-05-18 19:53:52 +0100

    Main.py

---
+++ Student Timetable 2.py
@@ -0,0 +1,41 @@
+'''This is my attempt at a student timetable
+5 students
+several subject'''
+
+subjects= ['Maths','Art','Science','Hstory','Geography','PE','English']
+
+username = input('enter your username')
+password = input('enter your password')
+
+if username =='Tom' and password =='Cartoon12':
+    print('Welcome Tom')
+    print('Your selected lessons are:')
+    print(subjects[0])
+    print(subjects[3])
+    print(subjects[-1])
+elif username =='Katie' and password =='python99':
+    print('Welcome katie')
+    print('Your selected lessons are:')
+    print(subjects[1])
+    (subjects[-2])
+    (subjects[-3])
+elif username =='Miles' and password=='Tails89':
+    print('Welcome Miles')
+    print('Your selected lessons are:')
+    print(subjects[-4])
+    print(subjects[-1])
+    print(subjects[0])
+elif username=='Ravi' and password=='Dance91':
+    print('Welcome Ravi')
+    print('Your selected lessons are:')
+    print(subjects[1])
+    print(subjects[0])
+    print(subjects[-3])
+elif username=='Fred' and password=='Buya01':
+    print('Welcome Fred')
+    print('Your selected lessons are:')
+    print(subjects[0])
+    print(subjects[-2])
+    print(subjects[1])
+else:
+    print('invalid login')
